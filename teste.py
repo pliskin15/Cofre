7347e9ea9153dcaf3c3eaa8bf1ca8796a9af8b86
pliskin15 <pauloteles15@outlook.com>
2026-05-19 17:27:17 -0400

    reset branch updates

--- teste.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('cofre.db')
-rows = conn.execute(
-    "SELECT data_deposito, data_hora, valor FROM depositos_prossegur WHERE loja = '49' AND data_deposito = '13/04/2026' ORDER BY data_hora"
-).fetchall()
-for r in rows:
-    print(r)
-conn.close()
